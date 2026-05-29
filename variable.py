70a172091bd4ade54c39b4bc2529425c92a033cd
Aditya2007raj <aadiraj2007singh@gmail.com>
2026-05-29 22:37:41 +0530

    Variables

--- variable.py
+++ variable.py
@@ -1,2 +1,20 @@
-print("Hello")
-print("Hello")
+# variable: something that stores a value.
+# variable is like a container jo bss ek baar m ek hii value ko store krta h. Or hmare pass ek naam ka ek hii variable hoga.
+
+# Rules to make a variable:
+# 1. Ek naam ka ek hii variable hoga.
+# 2. variable k name k beech m koi space nhi hoga.
+# 3. variable ka name (a-z) (A-Z) ya under score(_) se start hoga.
+# 4. baad m hm chahe jo lgana h lga do.
+# 5. Python is a case sensitive language tho yha pe variables bhi vaise hii call kiye jaaye ge.
+
+# Examples of variables:
+a= 1
+b=2
+print(a+b)
+
+table = 2
+print(table)
+
+Name_student = "Aditya"
+print(Name_student)
